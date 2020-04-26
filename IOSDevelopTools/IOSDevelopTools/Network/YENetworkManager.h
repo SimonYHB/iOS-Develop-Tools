115c3d87ccd1373c465100edda838e45c0e365fe
SimonYe <yehuangbin@hotmail.com>
2020-04-26 10:40:24 +0800

    update: 创建网络工具类

---
+++ IOSDevelopTools/IOSDevelopTools/Network/YENetworkManager.h
@@ -0,0 +1,17 @@
+//
+//  YENetworkManager.h
+//  IOSDevelopTools
+//
+//  Created by HuangBin Ye on 2020/4/26.
+//  Copyright © 2020 SimonYe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YENetworkManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ IOSDevelopTools/IOSDevelopTools/Network/YENetworkManager.m
@@ -0,0 +1,13 @@
+//
+//  YENetworkManager.m
+//  IOSDevelopTools
+//
+//  Created by HuangBin Ye on 2020/4/26.
+//  Copyright © 2020 SimonYe. All rights reserved.
+//
+
+#import "YENetworkManager.h"
+
+@implementation YENetworkManager
+
+@end
