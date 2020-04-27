e2d478c15969458b3e0102ed36ff12b2674262d7
SimonYe <yehuangbin@hotmail.com>
2020-04-27 09:18:54 +0800

    update: 更新网络框架

--- IOSDevelopTools/IOSDevelopTools/Network/YENetworkManager.h
+++ IOSDevelopTools/IOSDevelopTools/Network/YENetworkManager.h
@@ -10,7 +10,33 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+
+/**
+ 请求后返回的block
+ */
+typedef void(^YENetworkManagerResponseCallBack)(NSDictionary *response, NSDictionary *error);
+
+
+
 @interface YENetworkManager : NSObject
+
++ (nonnull instancetype)shareInstance;
+
+/**
+ *  网络请求
+ *  @param url             请求地址
+ *  @param paraDic         请求入参 {key: value}
+ *  @param method          请求类型 GET|POST
+ *  @param timeoutInterval 请求超时时间
+ *  @param headersDic      请求头 {key: value}
+ *  @param callBack        请求结果回调
+ */
+- (void)requestWithUrl:(NSString *)url
+                  body:(NSDictionary *)paraDic
+                method:(NSString *)method
+               timeOut:(NSTimeInterval)timeoutInterval
+               headers:(NSDictionary *)headersDic
+              callBack:(YENetworkManagerResponseCallBack)callBack;
 
 @end
 
--- IOSDevelopTools/IOSDevelopTools/Network/YENetworkManager.m
+++ IOSDevelopTools/IOSDevelopTools/Network/YENetworkManager.m
@@ -7,7 +7,96 @@
 //
 
 #import "YENetworkManager.h"
-
+#import "AFNetworking.h"
 @implementation YENetworkManager
+#pragma mark - Public
++ (nonnull instancetype)shareInstance {
+    static YENetworkManager *instance = nil;
+    static dispatch_once_t onceToken;
+    if (!instance) {
+        dispatch_once(&onceToken, ^{
+            instance = [[YENetworkManager alloc] init];
+        });
+    }
+    
+    return instance;
+}
 
+
+- (void)requestWithUrl:(NSString *)url
+                  body:(NSDictionary *)paraDic
+                method:(NSString *)method
+               timeOut:(NSTimeInterval)timeoutInterval
+               headers:(NSDictionary *)headersDic
+              callBack:(YENetworkManagerResponseCallBack)callBack {
+    // 参数异常处理
+    if (url == nil || url.length == 0)
+    {
+        if (callBack)
+        {
+            NSDictionary *errorDic = [NSDictionary dictionaryWithObject:@"URL为空" forKey:@"message"];
+            callBack(@{}, errorDic);
+        }
+        return;
+    }
+    if (method == nil || method.length == 0)
+    {
+        method = @"GET";
+    }
+    
+    
+}
+
+#pragma mark - Private
+
+- (AFHTTPSessionManager *)getHttpSessionManager:(NSDictionary *)configuration {
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/html", @"text/json", @"text/javascript",@"text/plain", nil];
+    // 设置configuration
+    return manager;
+}
+
+
+
+
+
+
+//处理HTTP响应携带的Cookie并存储
++ (void)storageHeaderFields:(NSDictionary *)headerFields forURL:(NSURL *)url
+{
+    NSArray *cookieArray = [NSHTTPCookie cookiesWithResponseHeaderFields:headerFields forURL:url];
+    if (cookieArray)
+    {
+        for (NSHTTPCookie *cookie in cookieArray)
+        {
+            NSHTTPCookieStorage *cookieStorage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
+            
+            //删除httponly的cookie，防止写入失败
+            for (NSHTTPCookie *storagedCookie in cookieStorage.cookies)
+            {
+                if (storagedCookie.isHTTPOnly && [cookie.name isEqualToString:storagedCookie.name] && [cookie.domain isEqualToString:storagedCookie.domain] && [cookie.path isEqualToString:storagedCookie.path])
+                {
+                    [cookieStorage deleteCookie:storagedCookie];
+                    break;
+                }
+            }
+            
+            [cookieStorage setCookie:cookie];
+        }
+    }
+}
+
+//匹配本地Cookie存储，获取对应URL的请求cookie字符串
++ (NSString*)getCookieHeaderForRequestURL:(NSURL*)requestURL
+{
+    NSArray *availableCookies = [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookiesForURL:requestURL];
+    if (availableCookies.count > 0)
+    {
+        NSDictionary *requestHeader = [NSHTTPCookie requestHeaderFieldsWithCookies:availableCookies];
+        return [requestHeader objectForKey:@"Cookie"];
+    }
+    return nil;
+}
 @end
